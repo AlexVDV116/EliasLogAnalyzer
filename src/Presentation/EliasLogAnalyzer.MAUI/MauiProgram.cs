02cd6b3d78793fe505ab60305f9bbaf869ce2a11
Alex <alexvdvelden@hotmail.com>
2024-04-06 18:56:38 +0200

    registered MainPage, MainViewModel and SettingsService dependencies in the dependency container

--- src/Presentation/EliasLogAnalyzer.MAUI/MauiProgram.cs
+++ src/Presentation/EliasLogAnalyzer.MAUI/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using EliasLogAnalyzer.MAUI.Services;
+using EliasLogAnalyzer.MAUI.ViewModels;
+using Microsoft.Extensions.Logging;
 
 namespace EliasLogAnalyzer.MAUI
 {
@@ -15,8 +17,13 @@ namespace EliasLogAnalyzer.MAUI
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
+            builder.Services.AddTransient<MainPage>();
+            builder.Services.AddTransient<MainViewModel>();
+            builder.Services.AddTransient<ISettingsService, SettingsService>();
+
+
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
