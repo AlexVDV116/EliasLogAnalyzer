f020982b618741015ff45d4aff63554de21886fa
Alex <alexvdvelden@hotmail.com>
2024-04-06 18:54:14 +0200

    created a settingsService to hold the appTheme

---
+++ src/Presentation/EliasLogAnalyzer.MAUI/Services/ISettingsService.cs
@@ -0,0 +1,14 @@
+﻿using EliasLogAnalyzer.MAUI.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EliasLogAnalyzer.MAUI.Services
+{
+    public interface ISettingsService
+    {
+        Theme AppTheme { get; set; }
+    }
+}
---
+++ src/Presentation/EliasLogAnalyzer.MAUI/Services/SettingsService.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using EliasLogAnalyzer.MAUI.Resources;
+
+namespace EliasLogAnalyzer.MAUI.Services
+{
+    public partial class SettingsService : ObservableObject, ISettingsService
+    {
+
+        public SettingsService()
+        {
+            // set the default appTheme to Light (in future this could be read from the preferences)
+            appTheme = Theme.Light;
+        }
+
+        [ObservableProperty]
+        public Theme appTheme;
+
+    }
+}
