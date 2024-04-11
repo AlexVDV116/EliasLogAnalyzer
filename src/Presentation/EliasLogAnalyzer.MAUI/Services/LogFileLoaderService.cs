922158d947e98526ebffa3faabc164a4c796d5bd
Alex <alexvdvelden@hotmail.com>
2024-04-11 17:30:21 +0200

    abstracted loading of LogFile logic into a service

---
+++ src/Presentation/EliasLogAnalyzer.MAUI/Services/LogFileLoaderService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EliasLogAnalyzer.MAUI.Services
+{
+    public class LogFileLoaderService : ILogFileLoaderService
+    {
+        public async Task<IEnumerable<FileResult>> LoadLogFilesAsync()
+        {
+            var customFileType = new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
+            {
+                { DevicePlatform.WinUI, new List<string> { ".log" } },
+                { DevicePlatform.MacCatalyst , new List<string> { "public.log" } }
+            });
+
+            var results = await FilePicker.PickMultipleAsync(new PickOptions
+            {
+                FileTypes = customFileType
+            });
+
+            // TODO: Implement the logic to parse each log file into a LogFile domain model and return an IEnumerable of the results
+            return results;
+        }
+    }
+}
