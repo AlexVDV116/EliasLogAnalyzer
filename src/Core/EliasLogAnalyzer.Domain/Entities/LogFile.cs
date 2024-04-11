8b40297f4fbc1b287c01064cbec398a287bd1885
Alex <alexvdvelden@hotmail.com>
2024-04-11 17:29:02 +0200

    created domain model for LogFile

---
+++ src/Core/EliasLogAnalyzer.Domain/Entities/LogFile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EliasLogAnalyzer.Domain.Entities
+{
+    internal class LogFile
+    {
+        // Create a domain model for a log file
+    }
+}
