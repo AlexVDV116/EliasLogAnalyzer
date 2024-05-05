deadc09f23d65631099c29529e592033ca87b507
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-05-05 21:29:10 +0200

    Moved commands for ThemeChanges to AppShell

--- src/Presentation/EliasLogAnalyzer.MAUI/AppShell.xaml.cs
+++ src/Presentation/EliasLogAnalyzer.MAUI/AppShell.xaml.cs
@@ -1,10 +1,37 @@
-﻿namespace EliasLogAnalyzer.MAUI
+﻿using CommunityToolkit.Mvvm.Input;
+using EliasLogAnalyzer.MAUI.Pages;
+using EliasLogAnalyzer.MAUI.Resources;
+using EliasLogAnalyzer.MAUI.Services.Contracts;
+
+namespace EliasLogAnalyzer.MAUI
 {
     public partial class AppShell : Shell
     {
-        public AppShell()
+        private readonly ISettingsService _settingsService;
+        
+        public IRelayCommand ChangeToDarkThemeCommand { get; }
+        public IRelayCommand ChangeToLightThemeCommand { get; }
+        public IRelayCommand ChangeToSystemThemeCommand { get; }
+        
+        public AppShell(ISettingsService settingsService)
         {
+            _settingsService = settingsService;
+            
             InitializeComponent();
+            Routing.RegisterRoute("mainPage", typeof(MainPage));
+            Routing.RegisterRoute("logfilesPage", typeof(LogFilesPage));
+            
+            ChangeToDarkThemeCommand = new RelayCommand(() => ApplyTheme(Theme.Dark));
+            ChangeToLightThemeCommand = new RelayCommand(() => ApplyTheme(Theme.Light));
+            ChangeToSystemThemeCommand = new RelayCommand(() => ApplyTheme(Theme.System));
+            
+            this.BindingContext = this;
+        }
+        
+        private void ApplyTheme(Theme theme)
+        {
+            _settingsService.AppTheme = theme;
+            Application.Current?.Dispatcher.Dispatch(() => { Application.Current.UserAppTheme = theme.AppTheme; });
         }
     }
 }
