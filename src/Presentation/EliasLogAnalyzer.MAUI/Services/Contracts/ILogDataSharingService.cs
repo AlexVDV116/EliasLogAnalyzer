4bbee4ab5af9372dc55259f061c50ef6e424cdf4
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-05-06 21:10:53 +0200

    added ObservableCollection of SelectedLogFiles

--- src/Presentation/EliasLogAnalyzer.MAUI/Services/Contracts/ILogDataSharingService.cs
+++ src/Presentation/EliasLogAnalyzer.MAUI/Services/Contracts/ILogDataSharingService.cs
@@ -7,4 +7,5 @@ public interface ILogDataSharingService
 {
     ObservableCollection<LogFile> LogFiles { get; set; }
     ObservableCollection<LogEntry> LogEntries { get; set; }
+    ObservableCollection<LogFile> SelectedLogFiles { get; set; }
 }
