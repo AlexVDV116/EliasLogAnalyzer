6a62bf671e8abbf15ffbc98279df482a15b633c1
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-05-06 16:39:16 +0200

    Added FileSize and Computer properties

--- src/Core/EliasLogAnalyzer.Domain/Entities/LogFile.cs
+++ src/Core/EliasLogAnalyzer.Domain/Entities/LogFile.cs
@@ -7,5 +7,7 @@ public partial class LogFile : ObservableObject
     [ObservableProperty]
     private bool isSelected;
     public string FileName { get; set; }
+    public long FileSize { get; set; }
+    public string Computer { get; set; }
     public IList<LogEntry> LogEntries { get; set; }
 }
