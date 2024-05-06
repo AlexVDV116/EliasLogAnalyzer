ac67382903a29d1e6e79a69ee2de1305b014638d
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-05-06 16:42:33 +0200

    added checkbox to header that selects all elements in listview

--- src/Presentation/EliasLogAnalyzer.MAUI/ViewModels/LogFilesViewModel.cs
+++ src/Presentation/EliasLogAnalyzer.MAUI/ViewModels/LogFilesViewModel.cs
@@ -9,9 +9,24 @@ namespace EliasLogAnalyzer.MAUI.ViewModels;
 public partial class LogFilesViewModel : ObservableObject
 {
     private readonly ILogDataSharingService _logDataSharingService;
+    private bool _selectAll;
+    
     public ObservableCollection<LogFile> LoadedLogFiles => _logDataSharingService.LogFiles;
     
     private IEnumerable<LogFile> SelectedLogFiles => LoadedLogFiles.Where(file => file.IsSelected).ToList();
+    
+    public bool SelectAll
+    {
+        get => _selectAll;
+        set
+        {
+            SetProperty(ref _selectAll, value);
+            foreach (var file in LoadedLogFiles)
+            {
+                file.IsSelected = _selectAll;
+            }
+        }
+    }
     
     public LogFilesViewModel(
         ILogDataSharingService logDataSharingService
@@ -28,4 +43,5 @@ public partial class LogFilesViewModel : ObservableObject
             Console.WriteLine($"Selected File: {file.FileName}");
         }
     }
+    
 }
