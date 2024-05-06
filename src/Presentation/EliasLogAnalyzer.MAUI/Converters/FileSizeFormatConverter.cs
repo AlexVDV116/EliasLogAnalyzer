fb77f7cfb6928e14b3f403a99910c558cfe5e046
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-05-06 16:38:39 +0200

    created FileSizeFormatConverter.cs

---
+++ src/Presentation/EliasLogAnalyzer.MAUI/Converters/FileSizeFormatConverter.cs
@@ -0,0 +1,28 @@
+using System.Globalization;
+
+namespace EliasLogAnalyzer.MAUI.Converters;
+
+public class FileSizeFormatConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not long bytes) return "0 Bytes";
+        const long kilobyte = 1024;
+        const long megabyte = 1024 * kilobyte;
+
+        switch (bytes)
+        {
+            case >= megabyte:
+                return $"{Math.Round(bytes / (double)megabyte, 2)} MB";
+            case >= kilobyte:
+                return $"{Math.Round(bytes / (double)kilobyte)} kB";
+            default:
+                return $"{bytes} B";
+        }
+    }
+    
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
