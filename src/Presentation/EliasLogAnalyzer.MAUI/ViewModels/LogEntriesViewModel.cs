d69dbee788781a3a5f4c26d4e5a1ef8d36966434
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-05-13 15:21:31 +0200

    Added sorting of LogEntries collection

--- src/Presentation/EliasLogAnalyzer.MAUI/ViewModels/LogEntriesViewModel.cs
+++ src/Presentation/EliasLogAnalyzer.MAUI/ViewModels/LogEntriesViewModel.cs
@@ -9,14 +9,44 @@ namespace EliasLogAnalyzer.MAUI.ViewModels;
 public partial class LogEntriesViewModel : ObservableObject
 {
     private readonly ILogDataSharingService _logDataSharingService;
-
-    public ObservableCollection<LogEntry> LogEntries { get; } = [];
+    
+    [ObservableProperty]
+    private ObservableCollection<string> _selectedLogEntryData = [];
 
     [ObservableProperty]
     private LogEntry _selectedLogEntry;
-
+    
     [ObservableProperty]
-    private ObservableCollection<string> _selectedLogEntryData = [];
+    private string _currentSortProperty;
+    
+    [ObservableProperty]
+    private string _sortHeader;
+    
+    [ObservableProperty]
+    private string _currentSortDirection;
+    
+    [ObservableProperty]
+    private string _ascendingText = "✓ Ascending";
+    
+    [ObservableProperty]
+    private string _descendingText = "Descending";
+    
+    private bool _ascending = true;
+    
+    private bool Ascending
+    {
+        get => _ascending;
+        set
+        {
+            if (SetProperty(ref _ascending, value))
+            {
+                AscendingText = value ? "✓ Ascending" : "Ascending";
+                DescendingText = !value ? "✓ Descending" : "Descending";
+            }
+        }
+    }
+    
+    public ObservableCollection<LogEntry> LogEntries { get; set; } = [];
 
     public LogEntriesViewModel(
         ILogDataSharingService logDataSharingService
@@ -24,6 +54,71 @@ public partial class LogEntriesViewModel : ObservableObject
     {
         _logDataSharingService = logDataSharingService;
         AggregateLogEntries();
+        SortByDateTime();
+    }
+    
+    private void UpdateSortHeader()
+    {
+        SortHeader = $"Sorting: \n{CurrentSortProperty} {(Ascending ? "Asc" : "Desc")}";
+    }
+    
+    [RelayCommand]
+    public void SetSortOrderAscending()
+    {
+        Ascending = true;
+    }
+    
+    [RelayCommand]
+    public void SetSortOrderDescending()
+    {
+        Ascending = false;
+    }
+    
+    [RelayCommand]
+    private void SortByDateTime()
+    {
+        if (Ascending)
+        {
+            LogEntries = new ObservableCollection<LogEntry>(
+                LogEntries.OrderBy(x => x.LogTimeStamp.DateTime)
+                    .ThenBy(x => x.LogTimeStamp.Ticks));
+        }
+        else
+        {
+            LogEntries = new ObservableCollection<LogEntry>(
+                LogEntries.OrderByDescending(x => x.LogTimeStamp.DateTime)
+                    .ThenByDescending(x => x.LogTimeStamp.Ticks));
+        }
+        OnPropertyChanged(nameof(LogEntries));
+        CurrentSortProperty = "DateTime";
+        CurrentSortDirection = Ascending ? "Ascending" : "Descending";
+        UpdateSortHeader();
+    }
+
+    [RelayCommand]
+    private void SortByProperty(string propertyName)
+    {
+        var propertyInfo = typeof(LogEntry).GetProperty(propertyName);
+
+        if (propertyInfo == null)
+        {
+            Console.WriteLine($"Property not found: {propertyName}");
+            return;
+        }
+
+        if (Ascending)
+        {
+            LogEntries = new ObservableCollection<LogEntry>(LogEntries.OrderBy(x => propertyInfo.GetValue(x, null)));
+        }
+        else
+        {
+            LogEntries = new ObservableCollection<LogEntry>(LogEntries.OrderByDescending(x => propertyInfo.GetValue(x, null)));
+        }
+
+        OnPropertyChanged(nameof(LogEntries));
+        CurrentSortProperty = propertyName;
+        CurrentSortDirection = Ascending ? "Ascending" : "Descending";
+        UpdateSortHeader();
     }
 
     private void AggregateLogEntries()
@@ -46,10 +141,8 @@ public partial class LogEntriesViewModel : ObservableObject
 
     private void UpdateSelectedLogEntryData()
     {
-
         SelectedLogEntryData.Clear();
-
-
+        
         if (SelectedLogEntry != null)
         {
 
