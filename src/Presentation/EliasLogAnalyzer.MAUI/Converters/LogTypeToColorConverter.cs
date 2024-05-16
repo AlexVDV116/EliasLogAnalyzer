6129e532e423395717244716c3ccadcf92bbf1f7
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2024-05-16 20:51:57 +0200

    changed colors to faded grid background colors

--- src/Presentation/EliasLogAnalyzer.MAUI/Converters/LogTypeToColorConverter.cs
+++ src/Presentation/EliasLogAnalyzer.MAUI/Converters/LogTypeToColorConverter.cs
@@ -7,23 +7,20 @@ namespace EliasLogAnalyzer.MAUI.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            var logType = value;
-
-            switch (logType)
+            if (value is LogType logType)
             {
-                case LogType.Error:
-                    return Colors.Red;
-                case LogType.Warning:
-                    return Colors.Orange;
-                case LogType.Information:
-                    return Colors.Green;
-                case LogType.Debug:
-                    return Colors.Black;
-                case LogType.None:
-                    return Colors.Gray;
-                default:
-                    return Colors.Gray; // Default color
+                return logType switch
+                {
+                    LogType.Error => Color.FromRgba(255, 0, 0, 0.1),        // Red with 20% opacity
+                    LogType.Warning => Color.FromRgba(255, 165, 0, 0.1),   // Orange with 20% opacity
+                    LogType.Information => Color.FromRgba(0, 255, 0, 0.1), // Green with 20% opacity
+                    LogType.Debug => Color.FromRgba(0, 0, 0, 0.1),         // Black with 20% opacity
+                    LogType.None => Color.FromRgba(128, 128, 128, 0.1),    // Gray with 20% opacity
+                    _ => Color.FromRgba(128, 128, 128, 0.1)                // Default to gray with 20% opacity
+                };
             }
+
+            return Color.FromRgba(128, 128, 128, 0.2); // Default Gray with 20% opacity
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
